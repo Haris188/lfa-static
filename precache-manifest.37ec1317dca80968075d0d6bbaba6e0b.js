2a68a1cd190bdf24ed792255b2fd31690f121b1f
= <=>
2024-06-16 18:58:39 -0400

    first commit

---
+++ precache-manifest.37ec1317dca80968075d0d6bbaba6e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910e76b39ffdeb4b835419721b9400d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d118e266545f1df560",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "85d118e266545f1df560",
+    "url": "/static/js/2.458e5f0c.chunk.js"
+  },
+  {
+    "revision": "e9e871edefa815884fc1239056db5653",
+    "url": "/static/js/2.458e5f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7ce2e7a6dee9ac45aa",
+    "url": "/static/js/main.697d2650.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  }
+]);
